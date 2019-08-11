b281bd034867a6963828112ceec2ef9251f0ca9b
Rekcahherry <792796011@qq.com>
2019-08-11 16:27:45 +0800

    这是一个github测试demo

---
+++ src/main/java/GitHubDemo/Demo1.java
@@ -0,0 +1,11 @@
+package GitHubDemo;
+
+/**
+ * @Author hla
+ * @Date 2019/8/11 9:42
+ **/
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("这是一个githubDemo");
+    }
+}
